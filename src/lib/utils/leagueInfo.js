ec135ddb80835e4d9619a3229c45f24fad0e19be
mybadreligon <58921417+mybadreligon@users.noreply.github.com>
2023-01-19 10:26:58 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -24,8 +24,9 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 
 export const managers = [
     {
-      "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-      "name": "Your Name",
+//       "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "name": "JBackes33",
+      "managerID": "76444495583264768",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
       "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Brooklyn", // (optional)
       "bio": "Lorem ipsum...",
